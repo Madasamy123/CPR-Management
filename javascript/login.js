9a1f0844f150667c8a6aa9b849bbb313701d8263
Madasamy123 <madasamimadasami2002@gmail.com>
2025-01-04 13:58:21 +0530

    some changes

--- javascript/login.js
+++ javascript/login.js
@@ -1,19 +1,19 @@
-document.getElementById("signup-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+// document.getElementById("signup-form").addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent the default form submission behavior
 
-    // Check which role is selected
-    const selectedRole = document.querySelector('input[name="role"]:checked').value;
+//     // Check which role is selected
+//     const selectedRole = document.querySelector('input[name="role"]:checked').value;
 
-    // Redirect based on the selected role
-    if (selectedRole === "students") {
-        window.location.href = "/pages/students.html"; // Redirect to the student page
-    }
-     else if (selectedRole === "teachers") {
-        window.location.href = "/pages/teachers.html"; // Redirect to the teacher page
-    }
-    else if(selectedRole === "lead"){
-        window.location.href = "/pages/lead.html"
-    }
+//     // Redirect based on the selected role
+//     if (selectedRole === "students") {
+//         window.location.href = "/pages/students.html"; // Redirect to the student page
+//     }
+//      else if (selectedRole === "teachers") {
+//         window.location.href = "/pages/teachers.html"; // Redirect to the teacher page
+//     }
+//     else if(selectedRole === "lead"){
+//         window.location.href = "/pages/lead.html"
+//     }
 
 
-});
+// });
